3a35b4e4354d471a638a3185f0710c6403c76477
kevcardenas99 <kevcardenas99@gmail.com>
2020-06-02 03:32:34 -0400

    changed to hamburger menu

--- js/indexScripts.js
+++ js/indexScripts.js
@@ -4,18 +4,23 @@ function colorMode(){
   dark = !dark;
   var element = document.getElementById("bg");
   element.classList.toggle("darkBgImage");
-  element = document.getElementById("navbar");
-  element.classList.toggle("bg-dark");
-  element.classList.toggle("navbar-dark");
   element = document.getElementById("aboutMe");
   element.classList.toggle("darkModeAbout");
   element = document.getElementById("projects");
   element.classList.toggle("darkModeProjects");
   if(dark){
     document.getElementById("githubIcon").src= "./img/GitHub-Mark-120px-plus.png";
+    document.getElementById("footerGithubIcon").src= "./img/GitHub-Mark-120px-plus.png";
+    document.getElementById("switch").innerHTML="Switch to Light Mode:";
+    document.getElementById("splash").style.background = "rgba(0,0,0,.9)";
+    document.getElementById("navLinks").style.color = "white";
   }
   else{
     document.getElementById("githubIcon").src= "./img/GitHub-Mark-Light-120px-plus.png";
+    document.getElementById("footerGithubIcon").src= "./img/GitHub-Mark-Light-120px-plus.png";
+    document.getElementById("switch").innerHTML="Switch to Dark Mode:";
+    document.getElementById("splash").style.background = "rgba(255,255,255,.9)";
+    document.getElementById("navLinks").style.color = "black";
   }
 }
 
